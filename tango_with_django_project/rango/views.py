53b76a269777da5ae370656a6776edd614dfac88
Paulius Jonusas <pauliusjonusas@gmail.com>
2015-03-08 22:11:57 +0000

    Chapter 16 + 17 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rango.models import Category, Page
-from rango.forms import UserForm, UserProfileForm, CategoryForm, PageForm
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
+from rango.forms import CategoryForm, PageForm
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 from rango.bing_search import run_query
@@ -13,113 +11,86 @@ def restricted(request):
     context_dict = {}
     return render(request, 'rango/restricted.html', context_dict)
 
-def index(request):
 
+def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
     context_dict = {'categories': category_list, 'pages': page_list}
-
     visits = request.session.get('visits')
     if not visits:
         visits = 1
     reset_last_visit_time = False
-
     last_visit = request.session.get('last_visit')
     if last_visit:
         last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-
         if (datetime.now() - last_visit_time).seconds > 0:
-            # ...reassign the value of the cookie to +1 of what it was before...
             visits = visits + 1
-            # ...and update the last visit cookie, too.
             reset_last_visit_time = True
     else:
-        # Cookie last_visit doesn't exist, so create it to the current date/time.
         reset_last_visit_time = True
-
     if reset_last_visit_time:
         request.session['last_visit'] = str(datetime.now())
         request.session['visits'] = visits
     context_dict['visits'] = visits
-
-
     response = render(request,'rango/index.html', context_dict)
-
     return response
 
+
 def about(request):
-    # If the visits session varible exists, take it and use it.
-    # If it doesn't, we haven't visited the site so set the count to zero.
     if request.session.get('visits'):
         count = request.session.get('visits')
     else:
         count = 0
-
-    # remember to include the visit data
     return render(request, 'rango/about.html', {'visits': count})
+
 
 def category(request, category_name_slug):
     context_dict = {}
+    context_dict['result_list'] = None
+    context_dict['query'] = None
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:
+            context_dict['result_list'] = run_query(query)
+            context_dict['query'] = query
+
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
-
-        # Retrieve all of the associated pages.
-        # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
+        pages = Page.objects.filter(category=category).order_by('-views')
         context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
-
         context_dict['category_name_url'] = category_name_slug
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything - the template displays the "no category" message for us.
         pass
 
-    # Go render the response and return it to the client.
+    if not context_dict['query']:
+        context_dict['query'] = category.name
+
     return render(request, 'rango/category.html', context_dict)
+
 
 @login_required
 def add_category(request):
-    # A HTTP POST?
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-
-        # Have we been provided with a valid form?
         if form.is_valid():
-            # Save the new category to the database.
             form.save(commit=True)
-
-            # Now call the index() view.
-            # The user will be shown the homepage.
             return index(request)
         else:
-            # The supplied form contained errors - just print them to the terminal.
             print form.errors
     else:
-        # If the request was not a POST, display the form to enter details.
         form = CategoryForm()
-
-    # Bad form (or form details), no form supplied...
-    # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
+
 
 @login_required
 def add_page(request, category_name_slug):
-
     try:
         cat = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
                 cat = None
-
     if request.method == 'POST':
         form = PageForm(request.POST)
         if form.is_valid():
@@ -128,7 +99,6 @@ def add_page(request, category_name_slug):
                 page.category = cat
                 page.views = 0
                 page.save()
-                # probably better to use a redirect here.
                 return category(request, category_name_slug)
         else:
             print form.errors
@@ -139,15 +109,33 @@ def add_page(request, category_name_slug):
 
     return render(request, 'rango/add_page.html', context_dict)
 
+
 def search(request):
-
     result_list = []
-
     if request.method == 'POST':
         query = request.POST['query'].strip()
-
         if query:
-            # Run our Bing function to get the results list!
             result_list = run_query(query)
-
     return render(request, 'rango/search.html', {'result_list': result_list})
+
+
+def track_url(request):
+    url = '/rango/'
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+            try:
+                page = Page.objects.get(id=page_id)
+                page.views += 1
+                page.save()
+                url = page.url
+            except:
+                pass
+    return redirect(url)
+
+
+@login_required
+def register_profile(request):
+    context_dict = {}
+    # Not yet implemented...
+    return render(request, 'profile_registration.html', context_dict)
